7ca90f6b2f6153b065b9159557e41a44be9e13eb
Christoffer G. Thomsen <chris@cgt.name>
2018-10-03 11:51:00 +0200

    Add master WCF service
    Boilerplate only, no functionality yet.

---
+++ PasswordCrackerMaster/PasswordCrackerMaster/PasswordCrackerMasterService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PasswordCrackerMaster
+{
+    public class PasswordCrackerMasterService : IPasswordCrackerMasterService
+    {
+        public string Foo()
+        {
+            return "test service";
+        }
+    }
+}
