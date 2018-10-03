de353e1ca79d8c6c1d7725f7ac2b1dabb0a7fede
Christoffer G. Thomsen <chris@cgt.name>
2018-10-03 12:02:16 +0200

    Add slave WCF client

---
+++ PasswordCrackerSlave/PasswordCrackerSlave/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordCrackerSlave.MasterServiceReference;
+
+namespace PasswordCrackerSlave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var endpointName = "BasicHttpBinding_IPasswordCrackerMasterService";
+            var endpointAddress = args.FirstOrDefault();
+            if (string.IsNullOrEmpty(endpointAddress))
+            {
+                Console.WriteLine("Must provide endpoint address command line argument");
+                return;
+            }
+            var client = new PasswordCrackerMasterServiceClient(endpointName, endpointAddress);
+            Console.WriteLine($"Response from master: {client.Foo()}");
+        }
+    }
+}
