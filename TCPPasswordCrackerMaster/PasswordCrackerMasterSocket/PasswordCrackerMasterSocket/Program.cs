4604e5400cf13c5de1080fb30c7ef422db165bec
gdi3000 <dani056i@edu.easj.dk>
2018-10-10 10:03:44 +0200

    Initial protocol handling

--- TCPPasswordCrackerMaster/PasswordCrackerMasterSocket/PasswordCrackerMasterSocket/Program.cs
+++ TCPPasswordCrackerMaster/PasswordCrackerMasterSocket/PasswordCrackerMasterSocket/Program.cs
@@ -1,15 +1,47 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace PasswordCrackerMasterSocket
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
+            IPAddress ip = IPAddress.Parse("0.0.0.0");
+            TcpListener CS = new TcpListener(ip,6789);
+
+
+            CS.Start();
+            var client = CS.AcceptTcpClient();
+
+            var stream = client.GetStream();
+            var reader = new StreamReader(stream);
+
+            var line = reader.ReadLine();
+
+            var cmd = JsonConvert.DeserializeObject<Command>(line);
+
+           
+            string SendWords()
+            {
+
+
+                return "puha";
+            }
+
+            string GetWords()
+            {
+                return "puha";
+            }
+        
         }
     }
 }
